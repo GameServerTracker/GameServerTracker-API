92848999091b8c62c41bee213e81cc463b71927b
BliTz <60670940+BliTz037@users.noreply.github.com>
2023-01-28 22:49:23 +0100

    [ADD] route for Source server

--- src/source/source.controller.ts
+++ src/source/source.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger/dist/decorators';
+import { SourceService } from './source.service';
+import ServerTrackedDto from 'src/dto/serverTrackedDto';
 
+@ApiTags('Source')
 @Controller('source')
-export class SourceController {}
+export class SourceController {
+    constructor(private readonly service: SourceService) { }
+
+    @Get('/:address')
+    @ApiOperation({
+        summary: "Track a Source Server",
+        description: "Return a JSON response",
+    })
+    @ApiResponse({
+        status: 200,
+        schema: {
+            example: {
+                message: "Todo"
+            }
+        }
+    })
+    async trackServer(@Param() address: ServerTrackedDto): Promise<any> {
+        return this.service.trackServer(address);
+    }   
+}
--- src/source/source.service.ts
+++ src/source/source.service.ts
@@ -1,4 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Server } from '@fabricio-191/valve-server-query';
+import { Injectable, Logger } from '@nestjs/common';
+import ServerTrackedDto from 'src/dto/serverTrackedDto';
 
 @Injectable()
-export class SourceService {}
+export class SourceService {
+    async trackServer(serverTracked: ServerTrackedDto): Promise<any> {
+        const addressSplited: string[] = serverTracked.address.split(':');
+        const hostname = addressSplited[0];
+        const port: number = addressSplited[1] != undefined ? +addressSplited[1] : 27015;
+
+        try {
+            if (port < 0 || port > 65536 || isNaN(port))
+                throw (`Address ${serverTracked.address} has a bad port !`);
+            const server: Server = await Server({ ip: hostname, port, timeout: 3000 });
+            let data: any = await server.getInfo();
+            // Avoid throw TypeError: Do not know how to serialize a BigInt
+            data.steamID = String(data.steamID);
+            data.appID = String(data.appID);
+            data.gameID = String(data.gameID);
+            return data;
+        } catch (err: any) {
+            Logger.warn(`[Source server | ${serverTracked.address}] ${err.name}: ${err.message}`);
+            return {
+                hostname,
+                port,
+                online: false
+            };
+        }
+    }
+}
