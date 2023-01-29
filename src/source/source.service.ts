253f8f2ebb3d145724cd6b288d0ebf95abd6265a
BliTz <60670940+BliTz037@users.noreply.github.com>
2023-01-29 17:49:58 +0100

    [DELETE] example for ServerTrackedDto

--- src/source/source.service.ts
+++ src/source/source.service.ts
@@ -18,7 +18,62 @@ export class SourceService {
             data.steamID = String(data.steamID);
             data.appID = String(data.appID);
             data.gameID = String(data.gameID);
-            return data;
+            return { ...data, online: true, ping: server.lastPing };
+        } catch (err: any) {
+            Logger.warn(`[Source server | ${serverTracked.address}] ${err.name}: ${err.message}`);
+            return {
+                hostname,
+                port,
+                online: false
+            };
+        }
+    }
+
+    async trackPlayers(serverTracked: ServerTrackedDto): Promise<any> {
+        const addressSplited: string[] = serverTracked.address.split(':');
+        const hostname = addressSplited[0];
+        const port: number = addressSplited[1] != undefined ? +addressSplited[1] : 27015;
+
+        try {
+            if (port < 0 || port > 65536 || isNaN(port))
+                throw (`Address ${serverTracked.address} has a bad port !`);
+            const server: Server = await Server({ ip: hostname, port, timeout: 3000 });
+            let data: any = await server.getPlayers();
+            return {
+                hostname,
+                port,
+                online: true,
+                ping: server.lastPing,
+                playersOnline: data.length,
+                players: data
+            };
+        } catch (err: any) {
+            Logger.warn(`[Source server | ${serverTracked.address}] ${err.name}: ${err.message}`);
+            return {
+                hostname,
+                port,
+                online: false
+            };
+        }
+    }
+
+    async trackRules(serverTracked: ServerTrackedDto): Promise<any> {
+        const addressSplited: string[] = serverTracked.address.split(':');
+        const hostname = addressSplited[0];
+        const port: number = addressSplited[1] != undefined ? +addressSplited[1] : 27015;
+
+        try {
+            if (port < 0 || port > 65536 || isNaN(port))
+                throw (`Address ${serverTracked.address} has a bad port !`);
+            const server: Server = await Server({ ip: hostname, port, timeout: 3000 });
+            const rules: any = await server.getRules();
+            return {
+                hostname,
+                port,
+                online: true,
+                ping: server.lastPing,
+                rules
+            };
         } catch (err: any) {
             Logger.warn(`[Source server | ${serverTracked.address}] ${err.name}: ${err.message}`);
             return {
