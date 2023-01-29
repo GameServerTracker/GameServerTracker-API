329d00ca7e04bba7c3416682d1c4ed59ef70b071
BliTz <60670940+BliTz037@users.noreply.github.com>
2023-01-29 18:22:05 +0100

    [ADD] route for fiveM

--- src/fivem/fivem.controller.ts
+++ src/fivem/fivem.controller.ts
@@ -1,4 +1,28 @@
 import { Controller } from '@nestjs/common';
+import { FivemService } from './fivem.service';
+import { Get, Param } from '@nestjs/common/decorators';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import ServerTrackedDto from 'src/dto/serverTrackedDto';
 
+@ApiTags('FiveM')
 @Controller('fivem')
-export class FivemController {}
+export class FivemController {
+    constructor(private readonly service: FivemService) { }
+
+    @Get('/:address')
+    @ApiOperation({
+        summary: "Track a FiveM Server",
+        description: "Return a JSON response",
+    })
+    @ApiResponse({
+        status: 200,
+        schema: {
+            example: {
+                message: "Todo"
+            }
+        }
+    })
+    async trackServer(@Param() address: ServerTrackedDto): Promise<any> {
+        return this.service.trackServer(address);
+    }
+}
--- src/fivem/fivem.service.ts
+++ src/fivem/fivem.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import axios, { AxiosResponse } from 'axios';
+import ServerTrackedDto from 'src/dto/serverTrackedDto';
 
 @Injectable()
-export class FivemService {}
+export class FivemService {
+    async trackServer(serverTracked: ServerTrackedDto): Promise<any> {
+        try {
+            const response: AxiosResponse = await axios.get(`http://${serverTracked.address}/dynamic.json`, { timeout: 2000 });
+            return {
+                address: serverTracked.address,
+                online: true,
+                ...response.data
+            };
+        } catch (err: any) {
+            Logger.warn(`[FiveM server | ${serverTracked.address}] ${err.name}: ${err.message}`);
+            return {
+                address: serverTracked.address,
+                online: false
+            };
+        }
+    }
+}
