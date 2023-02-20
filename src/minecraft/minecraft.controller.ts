6977095a09eed8f48f14d11a3c81a41989b21793
Tom Rives <tom.rives@epitech.eu>
2023-02-20 15:36:02 +0900

    [ADD] CacheManager for Minecraft in progress

--- src/minecraft/minecraft.controller.ts
+++ src/minecraft/minecraft.controller.ts
@@ -1,13 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { CACHE_MANAGER, Controller } from '@nestjs/common';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger/dist/decorators';
 import { MinecraftService } from './minecraft.service';
-import { Get, Param } from '@nestjs/common/decorators';
+import { Get, Inject, Param } from '@nestjs/common/decorators';
 import ServerTrackedDto from 'src/dto/serverTrackedDto';
+import { Cache } from 'cache-manager';
 
 @ApiTags('Minecraft')
 @Controller('minecraft')
 export class MinecraftController {
-    constructor(private readonly service: MinecraftService) { }
+    constructor(
+        @Inject(CACHE_MANAGER) private cacheManager: Cache,
+        private readonly service: MinecraftService
+    ) { }
 
     @Get('/:address')
     @ApiOperation({
@@ -23,7 +27,16 @@ export class MinecraftController {
         }
     })
     async trackServer(@Param() address: ServerTrackedDto): Promise<any> {
-        return this.service.trackServer(address);
+        const cache: any = await this.cacheManager.get(`MC:${address.address}`);
+        let result: any;
+
+        if (cache)
+            return cache;
+        result = await this.service.trackServer(address);
+        result["cacheTime"] = Math.floor(Date.now() / 1000);
+        result["cacheExpire"] = Math.floor(Date.now() / 1000) + (5 * 60);
+        this.cacheManager.set(`MC:${address.address}`, result, 5 * 60 * 1000);
+        return result;
     }
 
     @Get('/query/:address')
